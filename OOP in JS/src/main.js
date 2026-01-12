bcc02082f07f2935537a91be88d41ff37be801a7
ZarrarATU <zarrarantiterrorxd@gmail.com>
2026-01-11 23:38:50 -0800

    starting...

---
+++ OOP in JS/src/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>OOP IN JAVASCRIPT</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
